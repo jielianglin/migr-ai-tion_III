e1532f74848ad0986f18ae2a7aea9c1e395cee07
Jielianglin <jielianglin821@gmail.com>
2021-12-27 17:11:12 +0100

    installed create-react-app and set up react-router

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Gallery from "./pages/Gallery";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/gallery" element={<Gallery />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
