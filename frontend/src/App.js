e53b6e48cea372f68d183c853d8b5dc5e304a301
Jielianglin <jielianglin821@gmail.com>
2021-12-28 18:18:54 +0100

    implemented navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/gallery" element={<Gallery />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/blog" element={<Blog />} />
+          <Route exact path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBIcon,
+    MDBCollapse
+} from 'mdb-react-ui-kit';
+
+import logo from "../pics/logo.png"
+
+export default function Navbar() {
+    const [showNavSecond, setShowNavSecond] = useState(false);
+
+    var styles1 = {
+        width: '35px',
+        marginLeft: '10px',
+    }
+
+    var styles2 = {
+        fontSize: '24px',
+        padding: '10px'
+    }
+
+    var styles3 = {
+        boxShadow: 'none',
+    }
+    return (
+        <MDBNavbar expand='lg' light style={styles3} >
+            <MDBContainer fluid>
+                <MDBNavbarBrand href='/'>
+                    <img src={logo} style={styles1} />
+                    <span style={styles2}> MIGR-AI-TION </span>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                    onClick={() => setShowNavSecond(!showNavSecond)}
+                >
+                    <MDBIcon icon='bars' fas />
+                </MDBNavbarToggler>
+                <MDBCollapse navbar show={showNavSecond}>
+                    <MDBNavbarNav>
+                        <MDBNavbarLink active aria-current='page' href='/Gallery'>
+                            Gallery
+                        </MDBNavbarLink>
+                        <MDBNavbarLink href='/Blog'>Blog</MDBNavbarLink>
+                        <MDBNavbarLink href='/About'>About Us</MDBNavbarLink>
+                        <MDBNavbarLink href='/Contact'>Contact</MDBNavbarLink>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBContainer>
+        </MDBNavbar>
+    );
+}
