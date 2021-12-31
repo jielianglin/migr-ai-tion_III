d3624f0d74ec9643d294bf0c3b288c48222f9bd1
Jielianglin <jielianglin821@gmail.com>
2021-12-31 14:39:46 +0100

    Implemented Submit Button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,27 +2,42 @@ import React from "react";
 
 import FileInput from "../components/FileInput";
 import TextInput from "../components/TextInput";
+import SubmitButton from "../components/SubmitButton";
+
 import Grid from '@mui/material/Grid';
-import { flexbox } from "@mui/system";
 
 export default function Home() {
 
     var style1 = {
-        margin: "25px",
+        margin: "50px",
+    }
+
+
+    var style2 = {
+        marginTop: "30px",
+        display: "block",
+        margin: "0auto",
         display: "flex",
-        justifyContent: "center"
+        justifyContent: "center",
     }
 
     return (
         <div style={style1}>
-            <Grid container spacing={1} columns={2}>
-                <Grid item xs={1}>
-                    <FileInput />
+            <div>
+                <Grid container spacing={1} columns={2}>
+                    <Grid item xs={1}>
+                        <FileInput />
+                    </Grid>
+                    <Grid item xs={1}>
+                        <TextInput />
+                    </Grid>
                 </Grid>
-                <Grid item xs={1}>
-                    <TextInput />
-                </Grid>
-            </Grid>
-        </div>
+            </div>
+
+            <div style={style2}>
+                <SubmitButton />
+            </div>
+
+        </div >
     );
 }
