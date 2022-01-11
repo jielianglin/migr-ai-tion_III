43200e5b7907de5c1cc1b1d9fb87b8bef0095f2e
Jielianglin <jielianglin821@gmail.com>
2022-01-11 17:38:44 +0100

    implemented popovers for functional icons

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -13,14 +13,25 @@ import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Popover from '@mui/material/Popover';
+import { useTheme } from '@mui/styles';
 
 import "./Home.css";
 
-// import { ThemeProvider } from '@miu/material/style';
 
 export default function Home() {
     const [discoveryMode, setDiscoveryMode] = React.useState(false);
     const [uploadMode, setUploadMode] = React.useState(false);
+    const [anchorElUpload, setAnchorElUpload] = React.useState(null);
+    const [anchorElDiscover, setAnchorElDiscover] = React.useState(null);
+
+    const theme = useTheme({
+
+        paper: {
+            backgroundColor: 'transparent',
+        }
+
+    })
 
     var style1 = {
         margin: "50px",
@@ -68,13 +79,37 @@ export default function Home() {
         justifyContent: "center",
     }
 
+    var style10 = {
+        fontSize: '60px',
+        color: "#535252",
+    }
+
     const showUpload = () => {
         setUploadMode(true);
     }
 
-    const showDiscovery = () => {
+    const showDiscover = () => {
         setDiscoveryMode(true);
     }
+
+    const showUploadPopover = (event) => {
+        setAnchorElUpload(event.currentTarget);
+    }
+
+    const hideUploadPopover = () => {
+        setAnchorElUpload(null);
+    };
+
+    const showDiscoverPopover = (event) => {
+        setAnchorElDiscover(event.currentTarget);
+    }
+
+    const hideDiscoverPopover = () => {
+        setAnchorElDiscover(null);
+    };
+    const openUploadPopover = Boolean(anchorElUpload);
+
+    const openDiscoverPopover = Boolean(anchorElDiscover);
 
     if (uploadMode) {
         return <UploadForm />
@@ -87,18 +122,75 @@ export default function Home() {
                     <div>
                         <Grid container spacing={0}>
                             <Grid item xs={12} md={6} style={style6} >
-                                <button style={style8} onClick={showUpload}><img src={upload} alt="upload" style={style5} class="uploadIcon" /> </button>
+                                <button style={style8} onClick={showUpload}><img src={upload} alt="upload" style={style5} className="uploadIcon" onMouseEnter={showUploadPopover} onMouseLeave={hideUploadPopover} /></button>
+                                <Popover
+                                    id="mouse-over-popover"
+                                    sx={{
+                                        pointerEvents: 'none',
+                                    }}
+                                    PaperProps={{
+                                        elevation: 0,
+                                        sx: {
+                                            backgroundColor:
+                                                'transparent'
+                                        }
+                                    }}
+                                    open={openUploadPopover}
+                                    anchorEl={anchorElUpload}
+
+                                    anchorOrigin={{
+                                        vertical: 'center',
+                                        horizontal: 'center',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'center',
+                                        horizontal: 'center',
+                                    }}
+                                    onClose={hideUploadPopover}
+                                    disableRestoreFocus
+                                >
+                                    <Typography sx={{ p: 1 }} style={style10}><b>Upload</b></Typography>
+                                </Popover>
                             </Grid>
                             <Grid item xs={12} md={6} style={style7}>
-                                <button style={style8} onClick={showDiscovery}><img src={discovery} alt="discovery" style={style5} class="discoveryIcon" /> </button>
+                                <button style={style8} onClick={showDiscover}><img src={discovery} alt="discovery" style={style5} className="discoveryIcon" onMouseEnter={showDiscoverPopover} onMouseLeave={hideDiscoverPopover} /></button>
+                                <Popover
+                                    id="mouse-over-popover"
+                                    sx={{
+                                        pointerEvents: 'none',
+
+                                    }}
+                                    PaperProps={{
+                                        elevation: 0,
+                                        sx: {
+                                            backgroundColor:
+                                                'transparent'
+                                        }
+                                    }}
+                                    open={openDiscoverPopover}
+                                    anchorEl={anchorElDiscover}
+                                    anchorOrigin={{
+                                        vertical: 'center',
+                                        horizontal: 'center',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'center',
+                                        horizontal: 'center',
+                                    }}
+
+                                    onClose={hideDiscoverPopover}
+                                    disableRestoreFocus
+                                >
+                                    <Typography sx={{ p: 1 }} style={style10}><b>Discover</b></Typography>
+                                </Popover>
                             </Grid>
                         </Grid>
                     </div>
                     <br />
                     <div>
                         <Typography >
-                            We are using Expressive-Arts Therapy and Applied Visual Anthropology techniques to collect an anti-colonial dataset about migration.
-                            The purpose is to inform the public, AI researchers and businesses about the importance of representing the diverse experiences in the development of emerging technologies. Through our User Research, we designed and developed a special image-annotation tool for
+                            We are using <b>Expressive-Arts Therapy</b> and <b>Applied Visual Anthropology</b> techniques to collect an anti-colonial dataset about migration.
+                            The purpose is to inform the public, AI researchers and businesses about the importance of representing the diverse experiences in the development of emerging technologies. Through our <b>User Research</b>, we designed and developed a special image-annotation tool for
                             communicating migrant experiences, as a way of disseminating personal and collective healing paradigms.
                             <br />
                             <br />
@@ -109,21 +201,21 @@ export default function Home() {
                     <Grid container spacing={2} style={style2}>
                         <Grid item xs={12} md={4} style={style9}>
                             <button style={style8}>
-                                <img src={HomeIcon} alt="#Home" class="homeIcon" />
+                                <img src={HomeIcon} alt="#Home" className="homeIcon" />
                                 <br />
                                 <Typography>#Home</Typography>
                             </button>
                         </Grid>
                         <Grid item xs={12} md={4} style={style9}>
                             <button style={style8}>
-                                <img src={SolidarityIcon} alt="#Solidarity" class="solidarityIcon" />
+                                <img src={SolidarityIcon} alt="#Solidarity" className="solidarityIcon" />
                                 <br />
                                 <Typography>#Solidarity</Typography>
                             </button>
                         </Grid>
                         <Grid item xs={12} md={4} style={style9}>
                             <button style={style8}>
-                                <img src={WellBeingIcon} alt="#Well-being" class="wellBeingIcon" />
+                                <img src={WellBeingIcon} alt="#Well-being" className="wellBeingIcon" />
                                 <br />
                                 <Typography>#Well-being</Typography>
                             </button>
