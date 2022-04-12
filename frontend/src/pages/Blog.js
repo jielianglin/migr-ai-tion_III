badc78a7f1d097c66d19f5e794036f556d8bb2fa
Jielianglin <jielianglin821@gmail.com>
2022-04-12 17:33:52 +0200

    installed python and fastapi and created user auth
    login with username and password

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 export default function Blog() {
-    var style1 = {
-        margin: "50px"
-    }
-
     return (
-        <div style={style1}>
-            Under construction.
+        <div>
+            This is the Blog page.
         </div>
     );
 }
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 export default function Contact() {
-    var style1 = {
-        margin: "50px"
-    }
-
     return (
-        <div style={style1}>
-            For questions or collaborations, email: info@migr-ai-tion.com
+        <div>
+            This is the Contact page.
         </div>
     );
 }
