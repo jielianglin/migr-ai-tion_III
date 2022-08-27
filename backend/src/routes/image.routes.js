456781efcf7b613c4e9c66722407593d339e13c9
Jielianglin <jielianglin821@gmail.com>
2022-08-27 21:34:47 +0200

    updated paths in routes

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const imageController = require("../controllers/image.controller");
 const imageUpload = require("../middleware/image.middleware");
 let routes = (app) => {
-    router.get("/image.controller", imageController.getImagesbyTags);
-    router.post("/image.controller", imageUpload.array('uploaded_image', 2), imageController.uploadImages);
+    router.get("/image", imageController.getImagesbyTags);
+    router.post("/image", imageUpload.array('uploaded_image', 2), imageController.uploadImages);
     return app.use("/", router);
 };
 module.exports = routes;
