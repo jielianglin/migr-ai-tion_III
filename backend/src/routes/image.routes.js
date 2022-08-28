7a00c35230e61bf971f57193ae352aa8179b38ab
Jielianglin <jielianglin821@gmail.com>
2022-08-28 20:56:15 +0200

    reimplemented second argument in post request

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const imageController = require("../controllers/image.controller");
 const imageUpload = require("../middleware/image.middleware");
+
 let routes = (app) => {
     router.get("/image", imageController.getImagesbyTags);
-    router.post("/image", imageController.uploadImages);
+    router.post("/image", imageUpload.array('uploaded_image', 2), imageController.uploadImages);
     return app.use("/", router);
 };
 module.exports = routes;
