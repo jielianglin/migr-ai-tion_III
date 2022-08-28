44a9bf79f83bc47a53acf6a2e2bd5f86c8d4bd9f
Hubert Chen <hubertchen21@gmail.com>
2022-08-28 14:30:23 -0400

    Use multer in images router

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -1,11 +1,15 @@
+var multer = require("multer");
+
 const express = require("express");
 const router = express.Router();
 
 const imageController = require("../controllers/image.controller");
 const imageUpload = require("../middleware/image.middleware");
+const upload = multer({dest: "./"});
+
 let routes = (app) => {
     router.get("/image", imageController.getImagesbyTags);
-    router.post("/image", imageController.uploadImages);
+    router.post("/image", upload.single("file"), imageController.uploadImages);
     return app.use("/", router);
 };
 module.exports = routes;
