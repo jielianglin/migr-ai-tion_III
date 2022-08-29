e7e8ccc15c48effedc3307772d002e3a2e9a2fc1
Jielianglin <jielianglin821@gmail.com>
2022-08-29 15:35:28 +0200

    debugging images routes and middleware
    -import multer into routes

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -1,12 +1,18 @@
+var multer = require("multer");
+
 const express = require("express");
 const router = express.Router();
 
 const imageController = require("../controllers/image.controller");
-const imageUpload = require("../middleware/image.middleware");
+const imageMiddleware = require("../middleware/image.middleware");
+const upload = multer({ dest: "./" });
 
 let routes = (app) => {
     router.get("/image", imageController.getImagesbyTags);
-    router.post("/image", imageUpload.array('uploaded_image', 2), imageController.uploadImages);
+    //below needs to be tested
+    router.post("/image", upload.array('uploaded_image', 2), imageMiddleware, imageController.uploadImages);
+    //below is working
+    // router.post("/image", upload.single("file"), imageController.uploadImages);
     return app.use("/", router);
 };
 module.exports = routes;
