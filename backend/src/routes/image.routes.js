ef8f794750ce05b089808ad54f258e4edd3f6d0a
Hubert Chen <hubertchen21@gmail.com>
2022-09-18 12:44:43 -0400

    Add some changes

--- backend/src/routes/image.routes.js
+++ backend/src/routes/image.routes.js
@@ -1,16 +1,16 @@
-var multer = require("multer");
+// var multer = require("multer");
 
 const express = require("express");
 const router = express.Router();
 
 const imageController = require("../controllers/image.controller");
 const imageMiddleware = require("../middleware/image.middleware");
-const upload = multer({ dest: "./" });
+// const upload = multer({ dest: "./" });
 
 let routes = (app) => {
     router.get("/image", imageController.getImagesbyTags);
     //below needs to be tested
-    router.post("/image", upload.array('uploaded_image', 2), imageMiddleware, imageController.uploadImages);
+    router.post("/image", imageMiddleware.single("file"), imageController.uploadImages);
     //below is working
     // router.post("/image", upload.single("file"), imageController.uploadImages);
     return app.use("/", router);
