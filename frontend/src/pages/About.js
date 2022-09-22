e57224ecaecbe1bd2e90517d051540ffe2493d8a
Jielianglin <jielianglin821@gmail.com>
2022-09-22 19:12:16 +0200

    Updated About Page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export default function About() {
     var style1 = {
         margin: "50px"
@@ -11,34 +12,29 @@ export default function About() {
 
     return (
         <div style={style1}>
-            <span className="About"><i>
-                The liminal character of migration experiences is our departure from the quantifying influences of computer vision upon visual culture, and our modes of perception and being.
-                Not only do we want to counter the hegemonic media narratives about migration and (post)colonial societies, but
-                we also want to develop new models for building knowledge, participation and interconnectivity.
-                For our team, this means going beyond superficial tropes of “diversity”, and delving deeper into inquiries of what knowledge is,
-                how it is created and who it serves. Our dream is to provide those who are underrepresented, a voice in the future media landscape.
+            <h2><i>
+                We research diversity and technological inclusion, in order to develop new tools for co-creating AI.
+                <br />
+                <br />
+                We want to bring opportunities for self-representation, especially to those who are currently underrepresented in Tech.
+
+                <br />
+                <br />
+                We use process-oriented methodologies, in order to build communities.
             </i>
-            </span>
+            </h2>
             <br />
             <br />
             <br />
-            <div><b>PROJECT CONTRIBUTORS</b></div>
+            <div><b>FOUNDERS</b></div>
             <br />
-            <div ><span style={style2}>Jie Liang Lin // <i>project lead</i></span> <br />is a Chinese-American artist, visual and media anthropologist
-                and coder based in Berlin. Her work focuses on then intersections of new technologies and migration phenomena,
-                memory and mediation. Her research areas include migration, practice-led and applied methodologies and digital ethnography.
-                As an artist, she is interested in the interplay between drawing and photography, and in de-lineating a creative
-                history of image-annotation, beginning with hand-tinted, black-and-white photographs to avant-garde filmmakers
-                such as Stan Brackhage. </div>
+            <div ><span style={style2}>Jie Liang Lin </span> <br />is a Chinese-American artist, visual and media anthropologist
+                and web developer, based in Berlin. Her work focuses on then intersections of new technologies, migration phenomena,
+                memory and mediation. Her research areas include migration, digital ethnography, and practice-led and applied methodologies.
+                She is interested in the interplay between drawing and photography, and in de-lineating a creative
+                history of image-annotation. </div>
             <br />
-            <div> <span style={style2}> Nuno Moreira // <i>user research</i></span><br />
-                is a Portuguese architect and UX Designer based in Berlin interested in bringing a
-                human-centered design approach to the digital world. He has worked on this project
-                since the first prototype as a UX Designer. He is responsible for research
-                from a user-centric perspective and understanding both personal processes
-                and communal exchange facilitated through the platform.</div>
-            <br />
-            <div> <span style={style2}>Yasaman Pischvaie // <i>expressive-arts therapist</i></span> <br />
+            <div> <span style={style2}>Yasaman Pishvaie </span> <br />
                 is a multidisciplinary Iranian artist and Expressive Arts therapist based in Potsdam, Germany.
                 Her art-based research is focused on investigating new forms of interconnection and belonging
                 at the bridges of the online and offline worlds by utilizing the transformational and curative
@@ -47,37 +43,46 @@ export default function About() {
                 <br />
             </div>
             <br />
-            <div> <span style={style2}> Flor Beatriz Turiace // <i>expressive-arts facilitator</i> </span><br />
-                is a Berlin-based, Argentinian professional dancer, choreographer and dance teacher certified
-                in Expressive Arts therapy. The main focus of her research is migration, diversity and integration.
-                She believes that positive social changes occur while strengthening human connection without denying
-                personal uniqueness. Since 2020 she has facilitated dance-therapy workshops for migrant female,
-                trans-female and non-binary people, delving into deeper themes and commonalities amongst
-                participants. She is the workshop lead for this project, and responsible for identifying core migration themes.
+            <div><b>TEAM</b></div>
+            <br />
+            <div> <span style={style2}> Laura de Paz // <i>Product Manager</i> </span><br />
+                is a Spanish, Berlin-based web developer and writer on neurodiversity.
+                She has a background in Classical Philology (ancient Greek and Latin) and German studies.
+                Her other interests include dance, visual arts and creative writing.
+                She is managing the product development of MIGR-AI-TION and growing the community.
+                With her work, she aims to promote technological inclusion and spectral design.
                 <br />
             </div>
             <br />
-            <div> <span style={style2}> Cristian Checchanin // <i>graphic design</i> </span><br />
-                is a Columbian-Italian graphic designer. He is currently based in Riyadh, Saudi Arabia
-                and has also lived and worked in Hong Kong as a graphic designer. He has been involved in this project since the
-                ideation phase, through early feedback and helping to create the name. He worked on the functional buttons and logo design.
-                Cristian is passionate about bringing ideas to life through design, and uses the principle of simplicity for his approach.
+            <div><b>PAST CONTRIBUTORS</b></div>
+            <br />
+            <div> <span style={style2}> Flor Beatriz Turiace // <i>Expressive-Arts Facilitation</i> </span><br />
+            </div>
+            <div> <span style={style2}> Nuno Moreira // <i>User Research</i> </span><br />
+            </div>
+            <div> <span style={style2}> Cristian Checchanin // <i>Graphic Design</i> </span><br />
             </div>
             <br />
             <br />
-            <span><b>THANK YOU (in alphabetial order) :</b></span>
+            <span><b>THANK YOU (in alphabetial order by last name) :</b></span>
             <br />
             <br />
+            Lola Abrera <br />
             Saptarishi Baksi <br />
             Paul Bochtler<br />
             Aladin Borioli<br />
             CeMig (Zentrum für Globale Migrationsstudien)<br />
+            Sofía Fontenla <br />
+            Safa Ghnaim (Tactical Tech)<br />
             Btari Galuh Chandraditya<br />
+            Hubert Chen <br />
             Jan Dix<br />
             Margit Hain<br />
             Adam Harvey<br />
             Yoohna Kim<br />
             Michael Kreil<br />
+            Jan Magayanes <br />
+            Jöran Mandik (Floating University)<br />
             Julia Manek<br />
             Ana Alvarez Monge (Migrapreneurs)<br />
             Prototype Fund Management Team <br />
@@ -88,12 +93,13 @@ export default function About() {
             <br />
             <br />
             <span className="description">
-                <i><b>Phase 1 of this project was developed in the context of Techlabs Berlin.
+                <i>
+                    ...
                     <br />
-                    Phase 2 was supported by Prototype Fund, Open Knowledge Foundation Germany & the Federal Ministry of Education and Research (BMBF).</b></i>
-                <br />
-                <b><i>Phase 3 is ad hoc :)</i></b></span>
+                    Supported by Prototype Fund, Open Knowledge Foundation Germany & the Federal Ministry of Education and Research (BMBF).
+                </i>
+            </span >
 
-        </div>
+        </div >
     );
 }
