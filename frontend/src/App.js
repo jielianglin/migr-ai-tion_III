8a86e48a13d4e06f4d87ed16ed69ec203be01458
Jielianglin <jielianglin821@gmail.com>
2023-05-10 11:55:54 +0200

    clean-up
    deleted old auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from "./components/Navbar";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
+import Navbar from "./components/navigation/Navbar";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import Login from './pages/auth/Login';
+import Success from './pages/auth/Success';
 
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Profile from "./pages/auth/Profile";
-// import BoardUser from "./pages/auth/BoardUser";
-// import BoardAdmin from "./pages/auth/BoardAdmin";
 
 const App = () => {
 
@@ -20,15 +14,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/blog" element={<Blog />} />
-          <Route exact path="/contact" element={<Contact />} />
-
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/profile" element={<Profile />} />
-          {/* <Route exact path="/user" element={<BoardUser />} />
-          <Route exact path="/admin" element={<BoardAdmin />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/success" element={<Success />} />
         </Routes>
       </Router>
     </div>
