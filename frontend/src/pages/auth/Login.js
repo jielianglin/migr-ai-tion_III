c74825989d6051ad9a2b7a94ebc5da3950bf3a2e
Jielianglin <jielianglin821@gmail.com>
2023-05-10 12:01:45 +0200

    added supabase user auth pages

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,112 +1,34 @@
-import React, { useState, useRef } from "react";
+import React from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
 import { useNavigate } from 'react-router-dom';
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import AuthService from "../../components/auth-services/auth.service";
-const required = (value) => {
-    if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This field is required!
-            </div>
-        );
-    }
-};
-const Login = () => {
-    let navigate = useNavigate();
-    const form = useRef();
-    const checkBtn = useRef();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState("");
-    const onChangeUsername = (e) => {
-        const username = e.target.value;
-        setUsername(username);
-    };
-    const onChangePassword = (e) => {
-        const password = e.target.value;
-        setPassword(password);
-    };
-    const handleLogin = (e) => {
-        e.preventDefault();
-        setMessage("");
-        setLoading(true);
-        form.current.validateAll();
-        if (checkBtn.current.context._errors.length === 0) {
-            AuthService.login(username, password).then(
-                () => {
-                    navigate("/profile");
-                    window.location.reload();
-                },
-                (error) => {
-                    const resMessage =
-                        (error.response &&
-                            error.response.data &&
-                            error.response.data.message) ||
-                        error.message ||
-                        error.toString();
-                    setLoading(false);
-                    setMessage(resMessage);
-                }
-            );
+
+const supabase = createClient(
+    "https://migr-ai-tion.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjdmppbndrdm53dmZ6cnJ0dnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzM0OTMsImV4cCI6MTk5Mzk0OTQ5M30.6f3PL6nchUfFG-11-ZRFbZFt8q_VUdIKPuBsGdFpxtc"
+);
+
+export default function Login() {
+    const navigate = useNavigate();
+    supabase.auth.onAuthStateChange(async (event) => {
+        if (event !== "SIGNED_OUT") {
+            // navigate('/success');
+            console.log('success')
         } else {
-            setLoading(false);
+            // navigate('/login');
+            console.log('login')
         }
-    };
+    })
     return (
-        <div className="row d-flex justify-content-center">
-            <div className="col-md-4">
-                <div className="card card-container">
-                    <img
-                        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                        alt="profile-img"
-                        className="profile-img-card"
-                    />
-                    <Form onSubmit={handleLogin} ref={form}>
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <Input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                value={username}
-                                onChange={onChangeUsername}
-                                validations={[required]}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <Input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                value={password}
-                                onChange={onChangePassword}
-                                validations={[required]}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-primary btn-block" disabled={loading}>
-                                {loading && (
-                                    <span className="spinner-border spinner-border-sm"></span>
-                                )}
-                                <span>Login</span>
-                            </button>
-                        </div>
-                        {message && (
-                            <div className="form-group">
-                                <div className="alert alert-danger" role="alert">
-                                    {message}
-                                </div>
-                            </div>
-                        )}
-                        <CheckButton style={{ display: "none" }} ref={checkBtn} />
-                    </Form>
-                </div>
-            </div>
+        <div className="App">
+            <header className="App-header">
+                {/* <Auth
+                    supabaseClient={supabase}
+                    theme="dark"
+                /> */}
+                Test
+            </header>
         </div>
     );
-};
-export default Login;
+}
+
---
+++ frontend/src/pages/auth/Success.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+
+const supabase = createClient(
+    "https://gcvjinwkvnwvfzrrtvti.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjdmppbndrdm53dmZ6cnJ0dnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzM0OTMsImV4cCI6MTk5Mzk0OTQ5M30.6f3PL6nchUfFG-11-ZRFbZFt8q_VUdIKPuBsGdFpxtc"
+);
+
+export default function Success() {
+    const [session, setSession] = useState(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+        return (
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+        )
+    }
+    else {
+        return (<div>Logged in!</div>)
+    }
+}
