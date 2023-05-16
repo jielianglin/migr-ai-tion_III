58c97b333265c9b6950486a8eeb6131d2fe078fd
Jielianglin <jielianglin821@gmail.com>
2023-05-16 17:03:14 +0200

    supabase userauth

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import { createClient } from '@supabase/supabase-js';
-import { Auth } from '@supabase/auth-ui-react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
 
 const supabase = createClient(
-    "https://migr-ai-tion.supabase.co",
+    "https://gcvjinwkvnwvfzrrtvti.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjdmppbndrdm53dmZ6cnJ0dnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzM0OTMsImV4cCI6MTk5Mzk0OTQ5M30.6f3PL6nchUfFG-11-ZRFbZFt8q_VUdIKPuBsGdFpxtc"
 );
 
-export default function Login() {
-    const navigate = useNavigate();
-    supabase.auth.onAuthStateChange(async (event) => {
-        if (event !== "SIGNED_OUT") {
-            // navigate('/success');
-            console.log('success')
-        } else {
-            // navigate('/login');
-            console.log('login')
-        }
-    })
-    return (
-        <div className="App">
-            <header className="App-header">
-                {/* <Auth
-                    supabaseClient={supabase}
-                    theme="dark"
-                /> */}
-                Test
-            </header>
-        </div>
-    );
-}
+export default function Success() {
+    const [session, setSession] = useState(null)
 
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+        return (
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+        )
+    }
+    else {
+        return (<div>Logged in!</div>)
+    }
+}
